afa8c6acf4f359901c3034fd902f12e2af3fb1aa
warnov <warnov@gmail.com>
2017-09-17 13:06:56 -0500

    Problem 2 Solved

---
+++ ProjectEuler/SolutionLoader/Problem3.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SolutionLoader
+{
+    class Problem3
+    {
+    }
+}
---
+++ ProjectEuler/SolutionLoader/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SolutionLoader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            var result = Problem2.SolveProblem();
+            watch.Stop();
+
+            Console.WriteLine($"{result} in {watch.ElapsedTicks} ticks");            
+            Console.ReadLine();
+
+        }
+    }
+}
